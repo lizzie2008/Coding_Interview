67137e55eff343eb23364a82dbf27eae9e0a8471
lizzie2008 <lizzie_hls@yeah.net>
2018-05-30 11:30:41 +0800

    modify comments

--- src/cn/lancel0t/examples/E16_Power.java
+++ src/cn/lancel0t/examples/E16_Power.java
@@ -1,7 +1,7 @@
 /**
  * 
- * 【剑指Offer】面试题16 ：数值的整数次方
- * 【  题目描述 】给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ * 【剑指Offer】	面试题16 ：数值的整数次方
+ * 【题目描述】	给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
  * 
  * @author lancel0t
  * @date 2018年5月22日
